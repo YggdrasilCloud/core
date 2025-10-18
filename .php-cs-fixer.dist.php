e5888601b180c4668a07a297fc9c5915e5a93689
Mathias STRASSER <contact@roukmoute.fr>
2025-10-18 23:54:39 +0200

    Configure php-cs-fixer to use function imports instead of backslash prefix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -10,6 +10,12 @@ return (new PhpCsFixer\Config())
         '@PhpCsFixer' => true,
         '@PhpCsFixer:risky' => true,
         'yoda_style' => false,
+        'native_function_invocation' => false, // Disable backslash prefix
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
     ])
     ->setRiskyAllowed(true)
     ->setFinder($finder)
