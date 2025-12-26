b77799f95b17a31c33ca734d8039f38b3cb27b2a
Mathias STRASSER <contact@roukmoute.fr>
2025-12-26 17:30:03 +0100

    Configure BypassFinals in PHPUnit bootstrap
    Enable BypassFinals globally for all tests, allowing mocking of
    final classes without runtime modifications.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,13 @@
 
 declare(strict_types=1);
 
+use DG\BypassFinals;
+
 // Composer autoloader
 require_once dirname(__DIR__).'/vendor/autoload.php';
+
+// Enable BypassFinals to allow mocking final classes in tests
+BypassFinals::enable();
 
 // Force test environment (also defined in phpunit.xml.dist)
 $_SERVER['APP_ENV'] = 'test';
